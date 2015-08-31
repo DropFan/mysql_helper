8ea7c8e577f613a2b970fc9ce099165eccad67f0
DropFan <DropFan@Gmail.com>
2015-08-31 15:22:55 +0800

    add test_pysql.py

--- model.py
+++ model.py
@@ -2,21 +2,43 @@
 # -*- coding:utf-8 -*-
 # author:Tiger <DropFan@Gmail.com>
 
-from pysql import pysql
-from var_dump import var_dump as dump
-from pprint import pprint
+from pysql import pysql as mdb
+
 
 class model(object):
     """docstring for model"""
 
-    tableName = ''
-    data = {}
-    datamodel = {}
+    # _data = {}
+    dataModel = {}
 
-    def __init__(self, id, **args):
+    def __init__(self, id=-1, **kwargs):
         super(model, self).__init__()
         # self.arg = arg
+        # print 'model'
+        # self.tableName = ''
+        # self.data = {}
+        # self.datamodel = {}
+        self.data = {}
 
+        if isinstance(id, int) and id != -1:
+            self.id = id
+            self.data = self.__fetch_by_id()
+        else:
+            for key in self.dataModel.keys():
+                self.data[key] =  self.dataModel[key]
+            del(self.data['id'])
+
+
+
+    def __fetch_by_id(self):
+        fields= ', '.join(['`%s`' % k for k in self.dataModel.keys()])
+        # sql = "SELECT %s FROM `%s` WHERE `id` = '%d'" % (fields, self.tableName, self.id)
+        db = mdb(db_host='localhost', db_user='root', db_pass='123456', db_name='test')
+
+        db.select(self.tableName, fields, where='`id` = %d' % self.id)
+        data = db.fetch_one_dict()
+        print 'fetch ',data
+        return data
 
     def insert(self):
         pass
@@ -27,81 +49,20 @@ class model(object):
     def delete(self):
         pass
 
-    def __del__(self):
-        pass
+    # def __getattr__(self, key):
+    #     print 'get',key
 
-class userModel(model):
-
-    tableName = 'user'
-
-    data = {
-        'id'        : 1,
-        'name'      : str,
-        'pass'      : str,
-        'email'     : str,
-        'reg_time'  : int,
-        'last_login': int
-    }
-    dataModel = {
-        'id'        : int,
-        'name'      : str,
-        'pass'      : str,
-        'email'     : str,
-        'reg_time'  : int,
-        'last_login': int
-    }
-
-    def __init__(self, id='', **args):
-        super(model, self).__init__()
-        if isinstance(id, int):
-            self.id = id
-            self.data = self.__fetch_by_id()
+    #     if key in self.data.keys():
+    #         super(model, self).__getattribute__(self.data[key])
+    #     else:
+    #         super(model, self).__getattribute__(key)
 
 
-    def __fetch_by_id(self):
-        fields= ', '.join(['`%s`' % k for k in self.data.keys()])
-        sql = "SELECT %s FROM `%s` WHERE `id` = '%d'" % (fields, self.tableName, self.id)
-        print sql
-
-    def __getattr__(self, key):
-        if key in self.data.keys():
-            return self.data[key]
-
-    def __setattr__ (self, key, value):
-        if key in self.data.keys() and isinstance(value, self.dataModel[key]):
-            self.data[key] = value
-        # print self.data
-
-
-
-
-def main():
-    print 'start main():'
-    print '---------------------------------------------'
-
-    print type(['k','a'])
-    user = userModel(1)
-    print '====='
-    print user.data
-    print '++++++++++'
-    # pprint(user)
-    # dump(user)
-
-    user2 = userModel()
-    print user
-    print user2
-
-    print user.data
-
-
-
-    print '---------------------------------------------'
-    print 'end main()'
-    pass
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+    # def __setattr__ (self, key, value):
+    #     print 'set k:',key,'v:',value
+    #     if key in self.data.keys() and isinstance(value, self.dataModel[key]):
+    #         print 'data[%s]:%s' %(key,value)
+    #         self.data[key] = value
+    #         super(model, self).__setattr__(key, value)
+    #     else:
+    #         super(model, self).__setattr__(key, value)

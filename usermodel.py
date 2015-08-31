a6b706b80b35c166e2f34ed6175a6508889dd443
DropFan <DropFan@Gmail.com>
2015-08-31 15:24:12 +0800

    add usermodel.py. just 4 test

---
+++ usermodel.py
@@ -0,0 +1,36 @@
+#! /bin/env python
+# -*- coding:utf-8 -*-
+# author: Tiger <DropFan@Gmail.com>
+
+from model import model
+
+class userModel(model):
+    """
+        user model extends model
+    """
+
+    tableName = 'user'
+
+    dataModel = {
+        'id'        : int,
+        'name'      : str,
+        'pass'      : str,
+        'email'     : str,
+        'reg_time'  : int,
+        'last_login': int
+    }
+
+    def __init__(self, id=-1, **kwargs):
+        # self.data = {
+        #     'id'        : int,
+        #     'name'      : str,
+        #     'pass'      : str,
+        #     'email'     : str,
+        #     'reg_time'  : int,
+        #     'last_login': int
+        # }
+        super(userModel, self).__init__(id)
+        # print self.data
+
+
+

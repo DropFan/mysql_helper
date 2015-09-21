b3a436918ab7b4ddd8dd6669b7adf46efbaf55d7
DropFan <DropFan@Gmail.com>
2015-09-21 10:22:54 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+#! /bin/env python
+# -*- coding:utf-8 -*-
+# author: Tiger <DropFan@Gmail.com>
+
+
+mysql_config = {
+    'db_host': 'localhost',
+    'db_port': 3306,
+    'db_user': 'root',
+    'db_pass': '123456',
+    'db_name': 'test',
+    'charset': 'utf8'
+}

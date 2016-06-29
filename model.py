9c35c314c8f934ac42a65721dc58f87993a35ab4
DropFan <DropFan@Gmail.com>
2016-06-29 16:15:24 +0800

    fix #! /usr/bin/env python2

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-#! /bin/env python2
+#! /usr/bin/env python2
 # -*- coding:utf-8 -*-
 # author:Tiger <DropFan@Gmail.com>
 
--- mysql_helper.py
+++ mysql_helper.py
@@ -1,4 +1,4 @@
-#! /usr/env python2
+#! /usr/bin/env python2
 # -*- coding:utf-8 -*-
 # author:Tiger <DropFan@Gmail.com>
 
